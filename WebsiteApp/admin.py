71b04c02215e880a5794f1eb139f1f8c933d305f
Mark Liakhovych <mark170706@gmail.com>
2022-07-02 17:49:35 +0300

    release!!!

--- WebsiteApp/admin.py
+++ WebsiteApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import FoodModel, FoodCategory
+from .models import FoodModel, FoodCategory, OrderModel, ReservationModel
 
 # Register your models here.
 admin.site.register(FoodModel)
 admin.site.register(FoodCategory)
+admin.site.register(OrderModel)
+admin.site.register(ReservationModel)
