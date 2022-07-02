8e82b88ce1db4a7583b8e70d8d796fc0d9f5cee7
Mark Liakhovych <mark170706@gmail.com>
2022-07-02 20:39:05 +0300

    pre-slider switch

--- WebsiteApp/static/js/slider.js
+++ WebsiteApp/static/js/slider.js
@@ -1,7 +1,9 @@
 let sliderIndex = 0;
 let maxTimer = 5000;
-let intervalT = 250;
+let intervalT = 250000;
 let timer = maxTimer;
+let direction = 'none';
+let prevId = 0;
 
 let dotc = $("#dots-con");
 for (let i = 0; i < $(".slide").length; i++) {
@@ -15,21 +17,35 @@ let changeSlide = () => {
     sliderIndex = 0;
 
   if (sliderIndex < 0)
-    sliderIndex = slides.length - 1;
+    sliderIndex = slides.length - 1
+
+  let prevSlide = slides[prevId];
+  let currSlide = slides[sliderIndex];
+
   for (let i = 0; i < slides.length; i++) {
     if (i === sliderIndex) continue;
-    setTimeout(() => {
-      slides[i].style.display = "none";
-    }, 500)
+    slides[i].style.display = "none";
     dots[i].classList.remove("active");
+    for (let class_ of ['slide_move_left', 'slide_move_right', 'slide_move_prev_left', 'slide_move_prev_right']) {
+      slides[i].classList.remove(class_);
+    }
   }
-  slides[sliderIndex].style.display = "block";
+
+  currSlide.style.display = "block";
+  prevSlide.style.display = "block";
+
+  if (direction != 'none') {
+    currSlide.classList.add(`slide_move_${direction}`);
+    prevSlide.classList.add(`slide_move_prev_${direction}`);
+  }
 
   dots[sliderIndex].classList.add("active");
   timer = maxTimer;
+  prevId = sliderIndex;
 }
 
 function slideMove(n) {
+  direction = n < 0 ? 'right' : 'left';
   sliderIndex += n;
   changeSlide();
 }
